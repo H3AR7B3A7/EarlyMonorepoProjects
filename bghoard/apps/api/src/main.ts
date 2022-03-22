f6c67d198352f93b2dcb465cb5fea8c9aeb0ab06
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2022-03-22 23:41:13 +0100

    🚀 Boardgame Hoard example

---
+++ bghoard/apps/api/src/main.ts
@@ -0,0 +1,24 @@
+import * as express from 'express';
+import { getGames } from './app/game';
+import { createReview, getReviews } from './app/review';
+import { addItemToCart, getCart, updateItemInCart } from './app/store';
+
+
+const app = express();
+app.use(express.json());
+
+app.get('/api/game', getGames);
+
+app.get('/api/review/:game', getReviews);
+app.post('/api/review/:game', createReview);
+
+app.get('/api/cart', getCart);
+app.post('/api/cart', addItemToCart);
+app.put('/api/cart', updateItemInCart);
+
+const port = process.env.PORT || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+
+server.on('error', console.error);
---
+++ bghoard/libs/api-interfaces/src/lib/api-interfaces.ts
@@ -0,0 +1,25 @@
+export interface Game {
+  name: string;
+  id: string;
+  image?: string;
+  description: string;
+  price: number;
+  rating?: number;
+  reviews?: Array<Review>;
+}
+
+export interface Test {
+  nums: Array<number>;
+  strings: Array<string>;
+}
+
+export interface Review {
+  game: string;
+  rating: number;
+  content: string;
+}
+
+export interface CartItem {
+  game: string;
+  quantity: number;
+}
